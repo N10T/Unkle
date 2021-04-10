e19630ce603a3bb122494b184aece95f12555721
N10T <andy.tisba@gmail.com>
2021-04-10 12:12:47 +0200

    function ok | perf nok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 function App() {
+  const [tip, setTip] = useState({content:"", position:[], width:0})
+console.log("rerender");
+  //initialisation of mouse move triggering
+  useEffect(() => {
+    const logData = e => {
+      e.target.dataset?.tips && console.log(e.target.dataset.tips,e.target.getBoundingClientRect())
+      
+      const boxElement = e.target.getBoundingClientRect()
+      setTip({
+        content: e.target.dataset.tips, 
+        position: [boxElement.x,boxElement.y+boxElement.height],
+        width:boxElement.width
+      })
+    }
 
-  React.useEffect(() => {
-
-    const logData = e => {e.target.dataset?.tips && console.log(e.target.dataset.tips)}
-    
     window.addEventListener("mousemove", logData);
-  
     return () => window.removeEventListener("mousemove", logData);
   }, []);
 
   return (
     <div className="App">
+      {tip.content && <div className="tips" style={{top:tip.position[1], left:tip.position[0],minWidth:tip.width}}>{tip.content}</div>}
       <h1>Technical test Unkle</h1>
       <form className="form" onSubmit={e=>e.preventDefault()}>
         <label htmlFor="name">Name
