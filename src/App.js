56deb00e9ca5037c8feccad88a147fe7d5a4e92f
N10T <andy.tisba@gmail.com>
2021-04-11 01:26:21 +0200

    function with different style

--- src/App.js
+++ src/App.js
@@ -1,35 +1,60 @@
-import './App.css';
-import React,{useState, useEffect} from 'react'
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const [tip, setTip] = useState({content:"", position:[], width:0})
-console.log("rerender");
+const setTooltip = (e, setTip, setTarget) => {
+  const { x, y, height, width } = e.target.getBoundingClientRect();
+  setTip(e.target.dataset.tips);
+  setTarget({ x, y, height, width });
+};
+let count = 0
+function MouseTrigger({ children, position = "bottom" }) {
+  const [tip, setTip] = useState("");
+  const [target, setTarget] = useState({ x: 0, y: 0, height: 0, width: 0 });
+
+  const { x, y, height, width } = target;
+
+  const style = {
+    top: { top: y - height, left: x },
+    right: { top: y, left: x + width },
+    bottom: { top: y + height, left: x },
+    left: { top: y, left: x - width },
+  }[position];
+  count ++
+console.log("render",count)
+  const ref = React.useRef(null);
   //initialisation of mouse move triggering
   useEffect(() => {
-    const logData = e => {
-      e.target.dataset?.tips && console.log(e.target.dataset.tips,e.target.getBoundingClientRect())
-      
-      const boxElement = e.target.getBoundingClientRect()
-      setTip({
-        content: e.target.dataset.tips, 
-        position: [boxElement.x,boxElement.y+boxElement.height],
-        width:boxElement.width
-      })
-    }
-
-    window.addEventListener("mousemove", logData);
-    return () => window.removeEventListener("mousemove", logData);
-  }, []);
+    const element =  ref.current;
+    element.addEventListener("mousemove", (e) => setTooltip(e, setTip, setTarget));
+    return () => element.removeEventListener("mousemove", setTooltip);
+  }, [ref]);
 
   return (
+    <>
+      {tip && (
+        <div className="tips" style={{ ...style, minWidth: width }}>
+          {tip}
+        </div>
+      )}
+      <div ref={ref}>{children}</div>
+    </>
+  );
+}
+
+function App() {
+  
+  return (
     <div className="App">
-      {tip.content && <div className="tips" style={{top:tip.position[1], left:tip.position[0],minWidth:tip.width}}>{tip.content}</div>}
       <h1>Technical test Unkle</h1>
-      <form className="form" onSubmit={e=>e.preventDefault()}>
-        <label htmlFor="name">Name
-        <input type="text" name="name" data-tips="write your name"/></label>
-        <button data-tips="send the form">Submit</button>
-      </form>
+      <MouseTrigger>
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="name">
+            Name
+            <input type="text" name="name" data-tips="write your name" />
+          </label>
+          <button data-tips="send the form">Submit</button>
+        </form>
+      </MouseTrigger>
     </div>
   );
 }
