7ebc5d7b2d49d523eeb2c5b8515422f05b581261
N10T <andy.tisba@gmail.com>
2021-04-10 11:09:17 +0200

    create minimum page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Technical test Unkle</h1>
+      <form className="form" onSubmit={e=>e.preventDefault()}>
+        <label htmlFor="name">Name
+        <input type="text" name="name" data-tips="write your name"/></label>
+        <button data-tips="send the form">Submit</button>
+      </form>
     </div>
   );
 }
