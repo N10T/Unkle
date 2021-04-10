2b4897686d4a08caf56fdb36f4a8a942168a55aa
N10T <andy.tisba@gmail.com>
2021-04-11 01:40:00 +0200

    fix on mouse leave => reset tip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+let count = 0
 
-const setTooltip = (e, setTip, setTarget) => {
+const setTooltip = (e, setTip, setTarget,setMousePosition) => {
   const { x, y, height, width } = e.target.getBoundingClientRect();
   setTip(e.target.dataset.tips);
   setTarget({ x, y, height, width });
+  setMousePosition({x:e.clientX,y:e.clientY})
 };
-let count = 0
-function MouseTrigger({ children, position = "bottom" }) {
+
+function MouseTrigger({ children, position="free" }) {
   const [tip, setTip] = useState("");
   const [target, setTarget] = useState({ x: 0, y: 0, height: 0, width: 0 });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0});
 
   const { x, y, height, width } = target;
 
@@ -18,15 +21,22 @@ function MouseTrigger({ children, position = "bottom" }) {
     right: { top: y, left: x + width },
     bottom: { top: y + height, left: x },
     left: { top: y, left: x - width },
+    free: { top: mousePosition.y + 3, left: mousePosition.x + 3 },
   }[position];
   count ++
 console.log("render",count)
   const ref = React.useRef(null);
-  //initialisation of mouse move triggering
+  //initialisation of mouse move triggering & mouse leaving
   useEffect(() => {
     const element =  ref.current;
-    element.addEventListener("mousemove", (e) => setTooltip(e, setTip, setTarget));
-    return () => element.removeEventListener("mousemove", setTooltip);
+    const mouseMoveHandler = (e) => setTooltip(e, setTip, setTarget,setMousePosition)
+    const mouseLeaveHandler = () => setTip("")
+    element.addEventListener("mousemove", mouseMoveHandler);
+    element.addEventListener("mouseleave", mouseLeaveHandler);
+    return () => {
+      element.removeEventListener("mousemove", mouseMoveHandler)
+      element.removeEventListener("mousemove", mouseLeaveHandler)
+    };
   }, [ref]);
 
   return (
@@ -46,7 +56,7 @@ function App() {
   return (
     <div className="App">
       <h1>Technical test Unkle</h1>
-      <MouseTrigger>
+      <MouseTrigger position="free">
         <form className="form" onSubmit={(e) => e.preventDefault()}>
           <label htmlFor="name">
             Name
